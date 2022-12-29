a5f8186bbabc1ee0cbaacc48b81461ad5952cfd0
LAPTOP-LMHGTNO8\Sophie <sadrozd@edu.hse.ru>
2022-12-29 07:39:01 +0300

    Creating the structure of class FileManager.

--- src/file_hierarchy/file/FileManager.java
+++ src/file_hierarchy/file/FileManager.java
@@ -1,4 +1,47 @@
 package file_hierarchy.file;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+
 public class FileManager {
+    // FileManager manages some folder.
+    private final String folder;
+
+
+    /*
+    Constructor of a FileManager, which is used to work with a given path;
+     */
+    public FileManager(String folder) {
+        this.folder = folder;
+    }
+
+
+    /*
+    Walk through all the files in the given folder and its subfolders and
+    create the list of paths.
+     */
+    public List<Path> getFilePaths() {
+        try (Stream<Path> paths = Files.walk(Paths.get(folder))) {
+            return paths
+                    .filter(Files::isRegularFile)
+                    .toList();
+        } catch (IOException e) {
+            System.out.println("There was an error while walking through the folder");
+            return new ArrayList<Path>();
+        }
+    }
+
+
+    /*
+    Method for concatenating all the files if it's possible.
+     */
+    public void concatenateAll() {
+        //
+    }
 }
