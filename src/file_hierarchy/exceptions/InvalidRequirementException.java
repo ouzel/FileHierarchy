28bdb6db76c1522ce225f574080a3989ede4a0c7
LAPTOP-LMHGTNO8\Sophie <sadrozd@edu.hse.ru>
2022-12-30 12:31:11 +0300

    Updating custom exceptions

--- src/file_hierarchy/exceptions/InvalidRequirementException.java
+++ src/file_hierarchy/exceptions/InvalidRequirementException.java
@@ -1,19 +1,8 @@
 package file_hierarchy.exceptions;
 
-public class InvalidRequirementException extends Exception {
-    public InvalidRequirementException() {
-        super();
-    }
 
+public class InvalidRequirementException extends Exception {
     public InvalidRequirementException(String message) {
         super(message);
-    }
-
-    public InvalidRequirementException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public InvalidRequirementException(Throwable cause) {
-        super(cause);
     }
 }
--- src/file_hierarchy/exceptions/InvalidSortingException.java
+++ src/file_hierarchy/exceptions/InvalidSortingException.java
@@ -1,19 +1,8 @@
 package file_hierarchy.exceptions;
 
-public class InvalidSortingException extends Exception {
-    public InvalidSortingException() {
-        super();
-    }
 
+public class InvalidSortingException extends Exception {
     public InvalidSortingException(String message) {
         super(message);
-    }
-
-    public InvalidSortingException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public InvalidSortingException(Throwable cause) {
-        super(cause);
     }
 }
